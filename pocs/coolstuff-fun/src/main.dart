2c33dbf86c4335d71a7e5660b154b8c9f34bc888
diegopacheco <diego.pacheco.it@gmail.com>
2023-08-02 00:40:29 -0700

    cool stuff in dart

---
+++ pocs/coolstuff-fun/src/main.dart
@@ -0,0 +1,12 @@
+void main() {
+
+  const Object i = 3; // Where i is a const Object with an int value...
+  const list = [i as int]; // Use a typecast.
+  const map = {if (i is int) i: 'int'}; // Use is and collection if.
+  const set = {if (list is List<int>) ...list}; // ...and a spread.
+
+  print(i);
+  print(list);
+  print(map);
+  print(set);
+}
