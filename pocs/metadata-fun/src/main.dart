22bdc1043cf5f49fc6db6e57566bd4300ac0f81b
diegopacheco <diego.pacheco.it@gmail.com>
2023-08-02 00:42:28 -0700

    metadata fun

---
+++ pocs/metadata-fun/src/main.dart
@@ -0,0 +1,15 @@
+class Todo {
+  final String who;
+  final String what;
+
+  const Todo(this.who, this.what);
+}
+
+@Todo('Dash', 'Implement this function')
+void doSomething() {
+  print('Do something');
+}
+
+void main() {
+  doSomething();
+}
