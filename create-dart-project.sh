466386ecd3f7453d4ea4a3deda9031e207017759
diegopacheco <diego.pacheco.it@gmail.com>
2023-08-02 19:35:54 -0700

    dart script improvements

--- create-dart-project.sh
+++ create-dart-project.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
 mkdir src/
+mkdir test/
 touch src/main.dart
 touch run.sh
 chmod +x ./run.sh
+touch pubspec.yaml
+
+projectname=${PWD##*/}
+
+echo "name: $projectname
+description: >-
+  TBD
+version: 1.0.0
+homepage: https://github.com/diegopacheco/dart-playground/tree/main/pocs/$projectname
+documentation: https://github.com/diegopacheco/dart-playground/blob/main/pocs/$projectname/README.md
+
+environment:
+  sdk: '>=2.12.0 <3.0.0'
+  
+#dependencies:
+
+dev_dependencies:
+  test: '>=1.24.5 <2.0.0'" > pubspec.yaml
 
 echo "void main() {
   print('Hello, World!');
